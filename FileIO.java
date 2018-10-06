5e2e62d741c037b4a91c34e55e45d8c24e43055f
Aaron Musgrave <aaron@winthropdc.com>
2018-10-06 13:14:49 +0800

    De-coupled File IO

---
+++ FileIO.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.util.LinkedList;
+
+public class FileIO {
+    public static LinkedList<String> readFile(String filename) {
+        LinkedList<String> inputStrings = new LinkedList<>();
+        FileReader fileStrm = null;
+        BufferedReader reader;
+        int lineNo = 1;
+        String line;
+
+        try {
+            // Init file reader
+            fileStrm = new FileReader(filename);
+            reader = new BufferedReader(fileStrm);
+            line = reader.readLine();
+
+            // Skip header row
+            lineNo++;
+            line = reader.readLine();
+
+            // Loop through to read every line
+            while (line != null) {
+                // Add current line to linked list
+                inputStrings.add(line);
+
+                // Get next line
+                lineNo++;
+                line = reader.readLine();
+            }
+
+            reader.close();
+        }
+        catch(IOException ioException1) {
+            if (fileStrm != null) {
+                // Close file stream
+                try {
+                    fileStrm.close();
+                }
+                catch (IOException ioException2) {
+                    // No further action
+                }
+                System.out.println("Error reading file at line " + lineNo);
+                System.out.println(ioException1.getMessage());
+            }
+        }
+
+        return inputStrings;
+    }
+
+    public static void writeFile(LinkedList<String> outputStrings, String filename) {
+        // Init file write
+        FileWriter fileWrtr = null;
+        BufferedWriter out;
+
+        try {
+            fileWrtr = new FileWriter(filename, true);
+            out = new BufferedWriter(fileWrtr);
+
+            // Iterate through strings
+            for (String currString : outputStrings) {
+                out.write(currString);
+                out.newLine();
+            }
+
+            // Close
+            out.close();
+        }
+        catch (IOException ioException1) {
+            if (fileWrtr != null) {
+                try {
+                    // Close file
+                    fileWrtr.close();
+                }
+                catch (IOException ioException2) {
+                    // No further action
+                }
+                System.out .println("Error writing to file.");
+                System.out.println(ioException1.getMessage());
+            }
+        }
+    }
+}
--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.HashMap;
+import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,115 +12,57 @@ public class Main {
             System.exit(1);
         }
 
-        HashMap<String, Student> students = importStudents(args[0]);
-        writeFile(students, args[1]);
+        // Read input file
+        LinkedList<String> inputStrings = FileIO.readFile(args[0]);
+        HashMap<String, Student> students = processInput(inputStrings);
+
+        // Write output file
+        LinkedList<String> outputStrings = getOutput(students);
+        FileIO.writeFile(outputStrings, args[1]);
     }
 
-    private static HashMap<String, Student> importStudents(String filename) {
+    public static HashMap<String, Student> processInput(LinkedList<String> inputStrings) {
         HashMap<String, Student> students = new HashMap<>();
-        FileReader fileStrm = null;
-        BufferedReader reader;
-        int lineNo = 1;
-        String line;
-
-        try {
-            // Init file reader
-            fileStrm = new FileReader(filename);
-            reader = new BufferedReader(fileStrm);
-            line = reader.readLine();
-
-            // Skip header row
-            lineNo++;
-            line = reader.readLine();
-            
-            // Loop through to create students
-            while (line != null) {
-                // Process current line
-                students = updateStudents(students, line);
-                
-                // Get next line
-                lineNo++;
-                line = reader.readLine();
-            }
-
-            reader.close();
-        }
-        catch(IOException ioException1) {
-            if (fileStrm != null) {
-                // Close file stream
-                try {
-                    fileStrm.close();
-                }
-                catch (IOException ioException2) {
-                    // No further action
-                }
-                System.out.println("Error reading file at line " + lineNo);
-                System.out.println(ioException1.getMessage());
-            }
-        }
-
-        return students;
-    }
-    
-    private static HashMap<String, Student> updateStudents(HashMap<String, Student> students, String line) {
         Student currStudent;
-        String[] parts = line.split(",");
+        String[] parts;
         String fullname;
 
-        // Trim whitespace from all parts
-        for (int ii = 0; ii < parts.length; ii++) {
-            parts[ii] = parts[ii].trim();
+        for (String currString : inputStrings) {
+            // Split string
+            parts = currString.split(",");
+
+            // Trim whitespace from all parts
+            for (int ii = 0; ii < parts.length; ii++) {
+                parts[ii] = parts[ii].trim();
+            }
+
+            fullname = parts[0] + " " + parts[1];
+            currStudent = students.get(fullname);
+
+            // Check if student does not already exist
+            if (currStudent == null) {
+                currStudent = new Student();
+                currStudent.setFirstname(parts[0]);
+                currStudent.setSurname(parts[1]);
+            }
+
+            // Update Students
+            currStudent.addDetail(parts[2]);
+            students.put(fullname, currStudent);
         }
-
-        fullname = parts[0] + " " + parts[1];
-        currStudent = students.get(fullname);
-
-        // Check if student already exists
-        if (currStudent == null) {
-            currStudent = new Student();
-            currStudent.setFirstname(parts[0]);
-            currStudent.setSurname(parts[1]);
-        }
-
-        // Update Students
-        currStudent.addDetail(parts[2]);
-        students.put(fullname, currStudent);
 
         return students;
     }
 
-    private static void writeFile(HashMap<String, Student> students, String filename) {
+    public static LinkedList<String> getOutput(HashMap<String, Student> students) {
+        LinkedList<String> outputStrings = new LinkedList<>();
         Student currStudent;
-        // Init file write
-        FileWriter fileWrtr = null;
-        BufferedWriter out;
 
-        try {
-            fileWrtr = new FileWriter(filename, true);
-            out = new BufferedWriter(fileWrtr);
+        for (String currKey : students.keySet()) {
+            currStudent = students.get(currKey);
+            outputStrings.add(currStudent.toCSVRow());
+        }
 
-            // Iterate through students
-            for (String currName : students.keySet()) {
-                currStudent = students.get(currName);
-                out.write(currStudent.toCSVRow());
-                out.newLine();
-            }
-            
-            // Close
-            out.close();
-        }
-        catch (IOException ioException1) {
-            if (fileWrtr != null) {
-                try {
-                    // Close file
-                    fileWrtr.close();
-                }
-                catch (IOException ioException2) {
-                    // No further action
-                }
-                System.out .println("Error writing to file.");
-                System.out.println(ioException1.getMessage());
-            }
-        }
+        return outputStrings;
     }
 }
